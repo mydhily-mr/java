fa92b6aa7f6ebd8d17513927f5a99739d0aad65d
Mydhily <mydhily37@gmail.com>
2022-05-05 15:08:04 +0530

    updates
    updates

---
+++ JavaExamples/Core/src/day1/Identifiers.java
@@ -0,0 +1,25 @@
+package day1;
+
+public class Identifiers
+{
+    public static void main(final String[] args) {
+        final int x = 10;
+        final int virat = 18;
+        final int VIRAT = 2;
+        final int Virat = 3;
+        final int ViRaT = 40;
+        final int Integer = 5;
+        final int uishdiusahoisajudoiauoiauakxjlk = 55;
+        final int Exception = 0;
+        final String String = "String";
+        System.out.println(x);
+        System.out.println(virat);
+        System.out.println(VIRAT);
+        System.out.println(Virat);
+        System.out.println(ViRaT);
+        System.out.println(Integer);
+        System.out.println(uishdiusahoisajudoiauoiauakxjlk);
+        System.out.println(Exception);
+        System.out.println(String);
+    }
+}
---
+++ JavaExamples/Core/src/day1/Literals.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class Literals
+{
+    public static void main(final String[] args) {
+        final float x = 1.2345678E7f;
+        final float f = 1.2345678E7f;
+        System.out.println(f);
+    }
+}
